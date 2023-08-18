3be271e1cfd4c070eb99a30ee6d2a33d3d24baef
Karel Kvítek <karel.kvitek@seznam.cz>
2023-08-18 15:26:27 +0200

    Edit HamburgerMenu

---
+++ components/Navbar/HamburgerMenu/HamburgerMenu.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import HamburgerMenu from './HamburgerMenu'
+
+const meta: Meta<typeof HamburgerMenu> = {
+  component: HamburgerMenu,
+}
+
+export default meta
+type Story = StoryObj<typeof HamburgerMenu>
+
+export const Default: Story = {
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
--- components/Navbar/HamburgerMenu/HamburgerMenu.tsx
+++ components/Navbar/HamburgerMenu/HamburgerMenu.tsx
@@ -4,31 +4,36 @@ import Link from 'next/link'
 // Framer Motion
 import { motion } from 'framer-motion'
 
+// React Remove Scroll
+import { RemoveScroll } from 'react-remove-scroll'
+
 // Data
 import { MAIN_LINKS } from '../Navbar.constants'
 
 const HamburgerMenu = () => {
   return (
-    <motion.div
-      className='bg-custom-blue-800 fixed bottom-0 left-0 right-0 top-[110px] px-8 py-12'
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      <nav>
-        <ul className='flex flex-col items-center gap-8'>
-          {MAIN_LINKS.map(link => {
-            const { id, title, href } = link
+    <RemoveScroll>
+      <motion.div
+        className='bg-custom-blue-800 fixed bottom-0 left-0 right-0 top-[110px] overflow-auto px-8 pb-8 pt-10'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <nav>
+          <ul className='flex flex-col items-center gap-8'>
+            {MAIN_LINKS.map(link => {
+              const { id, title, href } = link
 
-            return (
-              <Link key={id} href={href} className='text-[2rem] text-white'>
-                <li>{title}</li>
-              </Link>
-            )
-          })}
-        </ul>
-      </nav>
-    </motion.div>
+              return (
+                <Link key={id} href={href} className='text-[2rem] text-white'>
+                  <li>{title}</li>
+                </Link>
+              )
+            })}
+          </ul>
+        </nav>
+      </motion.div>
+    </RemoveScroll>
   )
 }
 export default HamburgerMenu
