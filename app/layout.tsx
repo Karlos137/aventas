ce6c1ff11d501e48f32cedb534bebfb69eb9c8cb
Karel Kvítek <karel.kvitek@seznam.cz>
2023-09-06 09:56:12 +0200

    Edit GTM setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 // Global styles
 import './globals.css'
 
+// Next Google Tag Manager
+import GoogleTagManager from '@magicul/next-google-tag-manager'
+
 // Next.js
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,22 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='cs'>
-      <Script id='google-tag-manager' strategy='afterInteractive'>
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-      </Script>
       <body className={`${inter.className} app h-full`}>
+        <GoogleTagManager id={GTM_ID} />
         {children}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
-          }}
-        />
       </body>
     </html>
   )
