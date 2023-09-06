032312ce70f747c3c1d71721e0311dd0a4167702
Karel Kvítek <karel.kvitek@seznam.cz>
2023-09-06 09:50:53 +0200

    Added GTM script

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 // Next.js
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,8 @@ export const metadata: Metadata = {
   description: 'Advokátní kancelář Aventas.',
 }
 
+const GTM_ID = 'GTM-KK7RD957'
+
 export default function RootLayout({
   children,
 }: {
@@ -19,7 +22,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang='cs'>
-      <body className={`${inter.className} app h-full`}>{children}</body>
+      <Script id='google-tag-manager' strategy='afterInteractive'>
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+      <body className={`${inter.className} app h-full`}>
+        {children}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
+      </body>
     </html>
   )
 }
