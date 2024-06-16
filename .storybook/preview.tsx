456fc50ab4c55baee3b1b263715653cd7e65c9d8
Karlos137 <karel.kvitek@seznam.cz>
2024-06-16 08:32:19 +0200

    Add next intl provider to storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -9,6 +9,12 @@ import '../app/globals.css'
 // Next Font
 import { Inter } from 'next/font/google'
 
+// Next Intl
+import { NextIntlClientProvider } from 'next-intl'
+
+// Messages
+import messages from '../messages/cs.json'
+
 const inter = Inter({
   subsets: ['latin', 'latin-ext'],
   display: 'swap',
@@ -34,9 +40,11 @@ const preview: Preview = {
   },
   decorators: [
     Story => (
-      <div className={inter.className}>
-        <Story />
-      </div>
+      <NextIntlClientProvider locale='cs' messages={messages}>
+        <div className={inter.className}>
+          <Story />
+        </div>
+      </NextIntlClientProvider>
     ),
   ],
 }
