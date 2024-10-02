4427662a6d58f4ba1ec7be4dcfffe6e917103f1f
Karlos137 <karel.kvitek@seznam.cz>
2024-10-02 12:55:17 +0200

    fix misc

--- components/Navbar/Navbar.constants.ts
+++ components/Navbar/Navbar.constants.ts
@@ -15,8 +15,12 @@ export const MAIN_LINKS_CZ = [
     title: 'Náš tým',
     href: '/#nas-tym',
   },
-  { id: 5, title: 'Spolupracující subjekty', href: '#spolupracujici-subjekty' },
-  { id: 6, title: 'Reference', href: '#reference' },
+  {
+    id: 5,
+    title: 'Spolupracující subjekty',
+    href: '/#spolupracujici-subjekty',
+  },
+  { id: 6, title: 'Reference', href: '/#reference' },
   {
     id: 7,
     title: 'Kontakt',
@@ -41,7 +45,7 @@ export const MAIN_LINKS_EN = [
     title: 'Our team',
     href: '/#nas-tym',
   },
-  { id: 5, title: 'Collaborating entities', href: '#spolupracujici-subjekty' },
+  { id: 5, title: 'Collaborating entities', href: '/#spolupracujici-subjekty' },
   { id: 6, title: 'References', href: '/#reference' },
   {
     id: 7,
